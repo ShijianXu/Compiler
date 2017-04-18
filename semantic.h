efed8cf13a424fbcd98279800cb15362369d90c9
Xu Shijian <13260906215@163.com>
2017-04-19 00:12:50 +0800

    modification

--- semantic.h
+++ semantic.h
@@ -1,1 +1,22 @@
 void semantic_check(tree *root);
+
+typedef struct Type_* Type;
+typedef struct FieldList_* FieldList;
+
+struct Type_
+{
+	enum { BASIC, ARRAY, STRUCTURE } kind;
+	union
+	{
+		int basic;
+		struct { Type elem; int size; } array;	//数组类型信息
+		FieldList structure;	//结构体类型的信息是一个链表
+	} u;
+};
+
+struct FieldList_
+{
+	char *name;
+	Type type;		//type指针
+	FieldList next;	//next指针
+};
