53c1ad8bbe2724de74a878d2284690d933d3f2a4
Xu Shijian <13260906215@163.com>
2017-05-24 20:50:02 +0800

    preliminary

--- intercode.c
+++ intercode.c
@@ -5,8 +5,81 @@
 #include <assert.h>
 #include "intercode.h"
 
-void genInterCode(tree* root, FILE* fp)
+void init()
 {
-	fprintf(fp, "Hello, world! %d\n", 2);
-	fclose(fp);
+	IChead=(InterCodes)malloc(sizeof(struct InterCodes_));
+	
+	IChead->next = IChead;
+	IChead->prev = IChead;
+}
+
+void genInterCode(tree* root, FILE *fp)
+{
+	init();
+	dfs_(root);
+//	fprintf(fp, "Hello, world! %d\n", 2);
+//	fclose(fp);
+}
+
+void writeInterCode(FILE *fp)
+{
+	InterCodes pt = IChead;
+	do
+	{
+		pt = pt->next;
+	}
+	while(pt != IChead);
+}
+
+void insert_code(InterCodes code)
+{
+	//if()	
+}
+
+void translate_FunDec(tree* root)
+{
+	InterCodes code=(InterCodes)malloc(sizeof(struct InterCodes_));
+	code->icode.kind = FUNCTION_C;
+	
+	Operand op = (Operand)malloc(sizeof(struct Operand_));
+	op->kind = FUNCNAME;
+	tree* child = root->first_child;//ID LP VarList RP
+	strcpy(op->u.name, child->value);
+	
+	code->icode.u.funcall.op = op;
+
+	//VarList
+	insert_code(code);
+}
+
+void translate_CompSt(tree* root)
+{
+	
+}
+
+void dfs_(tree *root)
+{
+	if(strcmp(root->name, "ExtDef")==0)
+	{
+		tree* child = root->first_child->next_sibling;
+		if(strcmp(child->name,"ExtDecList")==0)
+		{
+			//DO NOTHING
+			//NO USE OF GLOBAL VAR
+		}
+		else if(strcmp(child->name,"SEMI")==0)
+		{
+			//感觉不需要翻译结构体
+		}
+		else if(strcmp(child->name,"FunDec")==0)
+		{
+			translate_FunDec(child);
+			child=child->next_sibling;	//CompSt
+			translate_CompSt(child);
+		}
+	}
+	if(root->first_child != NULL)
+		dfs_(root->first_child);
+	if(root->next_sibling != NULL)
+		dfs_(root->next_sibling);
 }
--- intercode.h
+++ intercode.h
@@ -4,34 +4,47 @@
 #include "semantic.h"
 
 typedef struct Operand_* Operand;
+typedef struct InterCodes_* InterCodes;
 
 struct Operand_
 {
-	enum { VARIABLE, CONSTANT, ADDRESS } kind;
+	enum { VARIABLE, CONSTANT, ADDRESS, FUNCNAME } kind;
 	union {
 		int var_no;
 		int value;
+		char name[40];
 		//...
 	} u;
 };
 
-struct InterCode
+struct Code
 {
-	enum { ASSIGN, ADD, SUB, MUL } kind;
+	enum { ASSIGN_C, ADD_C, SUB_C, MUL_C, DIV_C, RETURN_C, IF_GO, GOTO, LABEL, READ, WRITE, FUNCTION_C, ARG, PARAM, DEC } kind;
 	union {
 		struct { Operand right, left; } assign;
 		struct { Operand result, op1, op2; } binop;
+		struct { Operand op; } funcdec;
+		struct { Operand op; } funcall;
 		//...
 	} u;
 };
 
-struct InterCodes
+struct InterCodes_
 {
-	struct InterCode code;
-	struct InterCodes *prev, *next;
+	struct Code icode;
+	struct InterCodes_ *prev, *next;
 };
 
 
-void genInterCode(tree* root, FILE* fp);
+struct InterCodes_ *IChead;
+
+void init();
+void dfs_(tree* root);
+void writeInterCode(FILE *fp);
+void genInterCode(tree* root, FILE *fp);
+
+void insert_code(InterCodes);
+void translate_FunDec(tree* root);
+void translaet_CompSt(tree* root);
 
 #endif
